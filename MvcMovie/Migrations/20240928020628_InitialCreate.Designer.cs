9d0f422405eca48c5da39f61598f10256aa6934c
RossaMania <raclettenberg@gmail.com>
2024-09-27 21:12:45 -0500

    Delete the Migrations folder, and the databse file, then run commands for inital migration and update the database with that new column.

--- MvcMovie/Migrations/20240928020628_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20240928020628_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20240908025359_InitialCreate")]
+    [Migration("20240928020628_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -37,7 +37,10 @@ namespace MvcMovie.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<decimal>("Price")
-                        .HasColumnType("decimal(18,2)");
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("ReleaseDate")
                         .HasColumnType("datetime2");
